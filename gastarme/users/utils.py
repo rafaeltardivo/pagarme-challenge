88cb76318d8b4d2ac9117a6a18c56e8fd9bf4f8d
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-12 14:12:53 -0300

    Add utils to users

---
+++ gastarme/users/utils.py
@@ -0,0 +1,8 @@
+import re
+
+
+def is_alpha_or_space(value):
+    """Validates for only alphanumerics with/without spaces """
+
+    expression = re.compile("^[a-zA-Z ]*$")
+    return len(value) and expression.match(value)
