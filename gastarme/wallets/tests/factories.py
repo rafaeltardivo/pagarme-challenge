6839405ecd61259e9a0d50e75ef73696df5e4df4
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-13 11:07:36 -0300

    Add tests module to Wallets

---
+++ gastarme/wallets/tests/factories.py
@@ -0,0 +1,33 @@
+from datetime import date
+from decimal import Decimal
+
+import factory
+
+from wallets.models import CreditCard, Wallet
+from users.tests.factories import UserFactory
+
+
+class WalletFactory(factory.django.DjangoModelFactory):
+    """Factory for the model Wallet."""
+
+    class Meta:
+        model = Wallet
+
+    user = factory.SubFactory(UserFactory)
+    credit_limit = Decimal('750.00')
+    credit_available = Decimal('350.00')
+
+
+class CreditCardFactory(factory.django.DjangoModelFactory):
+    """ Factory for the model CreditCard """
+
+    class Meta:
+        model = CreditCard
+
+    wallet = factory.SubFactory()
+    number = '4729333912967715'
+    cardholder_name = "TEST USER CARD"
+    cvv = '333'
+    expires_at = date(2022, 10, 30)
+    monthly_billing_day = 10
+    limit = Decimal('500.00')
