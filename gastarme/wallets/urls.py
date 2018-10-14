0712082eefc2a4c0668ff8ea0c21c827861249c7
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-14 18:30:22 -0300

    Update urls order

--- gastarme/wallets/urls.py
+++ gastarme/wallets/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import WalletViewSet, CreditCardViewSet
 
 router = DefaultRouter()
-router.register('', WalletViewSet, 'wallets')
 router.register('creditcards', CreditCardViewSet, 'creditcards')
+router.register('', WalletViewSet, 'wallets')
 urlpatterns = router.urls
