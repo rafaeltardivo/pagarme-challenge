d2a42a4c80ac6d732faca55ede958264e740cbe2
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-14 18:36:26 -0300

    Add test to wallet match permission

--- gastarme/wallets/tests/test_permissions.py
+++ gastarme/wallets/tests/test_permissions.py
@@ -1,8 +1,11 @@
+from datetime import date
+from unittest.mock import patch
+
 from django.urls import reverse
 from rest_framework import test, status
 
 from users.models import User
-from wallets.models import Wallet
+from wallets.models import Wallet, CreditCard
 
 
 class TestWalletPermissions(test.APITransactionTestCase):
@@ -31,9 +34,7 @@ class TestWalletPermissions(test.APITransactionTestCase):
         self.assertEqual(Wallet.objects.count(), 0)
 
     def test_forbidden_superuser_wallet_update(self):
-        wallet = Wallet.objects.create(
-            user=self.user
-        )
+        wallet = Wallet.objects.create(user=self.user)
         self.client.force_authenticate(self.superuser)
 
         response = self.client.put(
@@ -43,9 +44,7 @@ class TestWalletPermissions(test.APITransactionTestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_forbidden_user_wallet_delete(self):
-        wallet = Wallet.objects.create(
-            user=self.user
-        )
+        wallet = Wallet.objects.create(user=self.user)
         self.client.force_authenticate(self.user)
 
         response = self.client.delete(
@@ -53,3 +52,25 @@ class TestWalletPermissions(test.APITransactionTestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @patch('wallets.logger.info')
+    def test_user_unauthorized_wallet(self, logger_mock):
+        wallet = Wallet.objects.create(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(
+            reverse('creditcards-list'),
+            {
+                'wallet': wallet.id + 1,
+                'cardholder_name': 'TEST USER ONE',
+                'number': '4729333912967715',
+                'cvv': '999',
+                'expires_at': '2022-10-30',
+                'monthly_billing_day': 9,
+                'limit': '900.00'
+            },
+            format='json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        logger_mock.assert_called()
