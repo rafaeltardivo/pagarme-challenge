10536491093972383fa7cbb46f73f15b94037c7c
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-16 15:13:15 -0300

    Update urls to nested

--- gastarme/wallets/urls.py
+++ gastarme/wallets/urls.py
@@ -1,8 +1,19 @@
-from rest_framework.routers import DefaultRouter
+from django.conf.urls import url, include
+from rest_framework_nested import routers
 
 from .views import WalletViewSet, CreditCardViewSet
 
-router = DefaultRouter()
-router.register('creditcards', CreditCardViewSet, 'creditcards')
-router.register('', WalletViewSet, 'wallets')
-urlpatterns = router.urls
+router = routers.SimpleRouter()
+router.register('', WalletViewSet, base_name='wallets')
+
+wallets_router = routers.NestedSimpleRouter(router, '', lookup='wallet')
+wallets_router.register(
+    'creditcards',
+    CreditCardViewSet,
+    base_name='wallet-creditcards'
+)
+
+urlpatterns = [
+    url('', include(router.urls)),
+    url('', include(wallets_router.urls)),
+]
