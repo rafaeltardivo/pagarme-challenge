c8addc9f51153426eade09dac0565df9ef4e6cbf
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-18 10:24:57 -0300

    Add signals to payment calculation

--- gastarme/wallets/serializers.py
+++ gastarme/wallets/serializers.py
@@ -105,4 +105,4 @@ class CreditCardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CreditCard
-        exclude = ('available',)
+        fields = '__all__'
--- gastarme/wallets/signals.py
+++ gastarme/wallets/signals.py
@@ -1,28 +1,53 @@
+from decimal import Decimal
+
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
+from django.db.models import Sum
+from django.db.models.signals import post_save
 
 from .models import CreditCard
-
-
-@receiver(pre_save, sender=CreditCard)
-def update_credit_available(sender, instance, **kwargs):
-    """Receiver for the Creditcard pre-save signal that will update
-       non-set available limits.
-    """
-
-    if not instance.limit:
-        instance.available = instance.limit
+from purchases.models import Purchase
 
 
 @receiver(post_save, sender=CreditCard)
-def update_wallet_credit_limit(sender, instance, **kwargs):
+def update_wallet_credit_limit(sender, instance, created, **kwargs):
     """Receiver for the Creditcard post-save signal that will update
        the CreditCard itself and it's related Wallet.
     """
-    related_wallet = instance.wallet
 
-    if not related_wallet.credit_limit:
-        related_wallet.credit_limit += instance.limit
-    if not related_wallet.credit_available:
-        related_wallet.credit_available += instance.limit
-    related_wallet.save()
+    if created:
+        # sets the limit to available
+        instance.available = instance.limit
+        instance.save()
+
+        # updates related wallet limit and available
+        related_wallet = instance.wallet
+        related_wallet.credit_limit += Decimal(instance.limit)
+        related_wallet.credit_available += Decimal(instance.limit)
+        related_wallet.save()
+
+
+@receiver(post_save, sender=Purchase)
+def payment(sender, instance, **kwargs):
+    """Receiver for the Creditcard post-save signal that will update
+       credit limits.
+    """
+    purchase_value = instance.value
+    wallet = instance.wallet
+    cards = wallet.credit_cards.all()
+
+    for card in cards:
+
+        if not purchase_value:
+            break
+
+        if purchase_value > card.available:
+            card.available = 0
+            purchase_value -= card.available
+        else:
+            card.available -= purchase_value
+            purchase_value = 0
+        card.save()
+
+    result = CreditCard.objects.aggregate(Sum('available'))
+    wallet.credit_available = result['available__sum']
+    wallet.save()
