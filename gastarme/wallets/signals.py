59283fb156ec65a95766795cec351cb5e7031c5d
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-18 23:55:59 -0300

    Update signals

--- gastarme/wallets/signals.py
+++ gastarme/wallets/signals.py
@@ -4,53 +4,40 @@ from django.dispatch import receiver
 from django.db.models import Sum
 from django.db.models.signals import post_save
 
-from .models import CreditCard
 from purchases.models import Purchase
+from .models import CreditCard
+from .services import set_payment_records
 
 
 @receiver(post_save, sender=CreditCard)
-def update_wallet_credit_limit(sender, instance, created, **kwargs):
+def update_wallet_post_create_card(sender, instance, created, **kwargs):
     """Receiver for the Creditcard post-save signal that will update
        the CreditCard itself and it's related Wallet.
     """
 
     if created:
-        # sets the limit to available
+        # sets the available limit
         instance.available = instance.limit
         instance.save()
 
         # updates related wallet limit and available
         related_wallet = instance.wallet
 
-        if not related_wallet.credit_available:
-            related_wallet.credit_available = Decimal('0.00')
-
+        related_wallet.credit_available = Decimal('0.00')
         related_wallet.credit_limit += Decimal(instance.limit)
         related_wallet.credit_available += Decimal(instance.limit)
         related_wallet.save()
 
 
 @receiver(post_save, sender=Purchase)
-def payment(sender, instance, **kwargs):
-    """Receiver for the Creditcard post-save signal that will update
+def update_wallet_post_purchase(sender, instance, **kwargs):
+    """Receiver for the Purchase post-save signal that will update
        credit limits.
     """
-    purchase_value = instance.value
     wallet = instance.wallet
     cards = wallet.credit_cards.all()
 
-    for card in cards:
-
-        if not purchase_value:
-            break
-
-        if purchase_value > card.available:
-            card.available = 0
-            purchase_value -= card.available
-        else:
-            card.available -= purchase_value
-            purchase_value = 0
-        card.save()
+    set_payment_records(cards, instance)
 
     result = CreditCard.objects.aggregate(Sum('available'))
     wallet.credit_available = result['available__sum']
