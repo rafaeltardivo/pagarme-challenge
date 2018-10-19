39ec7337ae11f00edf7a12f1529bc8e6a78b24e7
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-19 11:49:35 -0300

    Update purchases views

--- gastarme/purchases/views.py
+++ gastarme/purchases/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import MethodNotAllowed
 
 from commons.permissions import IsRegularUser
 
@@ -8,7 +9,7 @@ from .serializers import PurchaseSerializer
 from . import logger
 
 
-class PurchaseCreateView(CreateAPIView):
+class PurchaseViewSet(ModelViewSet):
     """Create view for purchases."""
     permission_classes = (IsRegularUser, )
     serializer_class = PurchaseSerializer
@@ -17,3 +18,17 @@ class PurchaseCreateView(CreateAPIView):
     def post(self, request, *args, **kwargs):
         logger.info("Purchase create request", extra={'user': request.user})
         return super().post(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        logger.info("Purchase list request", extra={'user': request.user})
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        logger.info("Wallet detail request", extra={'user': request.user})
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method)
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method)
