a076eea89724c1d4f2d7746261ae93058c14cd63
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-21 23:15:28 -0300

    Add bill model

--- gastarme/billings/models.py
+++ gastarme/billings/models.py
@@ -1,3 +1,30 @@
+from decimal import Decimal
+
 from django.db import models
 
-# Create your models here.
+from wallets.models import CreditCard
+
+
+class Bill(models.Model):
+    """Model for bills."""
+
+    credit_card = models.ForeignKey(
+        CreditCard,
+        on_delete=models.PROTECT,
+        related_name='purchases'
+    )
+    expires_at = models.DateTimeField()
+    value = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        default=Decimal('0.00')
+    )
+
+    def __str__(self):
+        return 'Credit card: {} expires_at: {} value: {}'.format(
+            self.credit_card.id, self.expires_at, self.value
+        )
+
+    class Meta:
+        ordering = ['id', ]
+        unique_together = ('credit_card', 'expires_at')
