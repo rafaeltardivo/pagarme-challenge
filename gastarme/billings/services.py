573b1f64b0699d0c23a613d362078cf2e65c3d8d
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-23 14:43:28 -0300

    Update billings services

--- gastarme/billings/services.py
+++ gastarme/billings/services.py
@@ -1,6 +1,10 @@
 from django.dispatch import Signal
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
 
-update_debt = Signal(providing_args=["value_paid", "bill"])
+from .models import Bill
+
+update_credit = Signal(providing_args=["value_paid", "bill"])
 
 
 def pay_bill(bill, value_paid):
@@ -9,4 +13,19 @@ def pay_bill(bill, value_paid):
     bill.value -= value_paid
     bill.save()
 
-    update_debt.send(bill.__class__, value_paid=value_paid, bill=bill)
+    update_credit.send(bill.__class__, value_paid=value_paid, bill=bill)
+
+
+def generate_or_obtain_bill(payment):
+    """Generates or obtains a bill for a payment."""
+
+    next_bill_date = timezone.now().date().replace(
+        day=payment.credit_card.monthly_billing_day
+    ) + relativedelta(months=1)
+
+    bill, _ = Bill.objects.get_or_create(
+        credit_card=payment.credit_card,
+        expires_at=next_bill_date
+    )
+    bill.value += payment.value
+    bill.save()
