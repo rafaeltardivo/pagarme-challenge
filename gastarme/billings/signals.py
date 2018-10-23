53915476c36d078e3d0f470e50eb108aa459b654
Rafael Tardivo <rafael.tardivo@cargobr.com>
2018-10-23 14:44:27 -0300

    Update signals

--- gastarme/billings/signals.py
+++ gastarme/billings/signals.py
@@ -1,10 +1,8 @@
-from django.utils import timezone
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from dateutil.relativedelta import relativedelta
 
 from purchases.models import Payment
-from .models import Bill
+from .services import generate_or_obtain_bill
 
 
 @receiver(post_save, sender=Payment)
@@ -12,13 +10,5 @@ def update_bill_post_payment(sender, instance, created, **kwargs):
     """ Receiver for the post-save payment signal that will update
         or generate bill for the Credit Card
     """
-    next_bill_date = timezone.now().date().replace(
-        day=instance.credit_card.monthly_billing_day
-    ) + relativedelta(months=1)
 
-    bill, _ = Bill.objects.get_or_create(
-        credit_card=instance.credit_card,
-        expires_at=next_bill_date
-    )
-    bill.value += instance.value
-    bill.save()
+    generate_or_obtain_bill(instance)
